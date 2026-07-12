e255bf90947be728122484f374bc7ee61503806d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "storage_mover_source_endpoints" {
-  description = "All storage_mover_source_endpoint resources"
-  value       = azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints
-}
 output "storage_mover_source_endpoints_description" {
-  description = "List of description values across all storage_mover_source_endpoints"
-  value       = [for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : v.description]
+  description = "Map of description values across all storage_mover_source_endpoints, keyed the same as var.storage_mover_source_endpoints"
+  value       = { for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : k => v.description }
 }
 output "storage_mover_source_endpoints_export" {
-  description = "List of export values across all storage_mover_source_endpoints"
-  value       = [for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : v.export]
+  description = "Map of export values across all storage_mover_source_endpoints, keyed the same as var.storage_mover_source_endpoints"
+  value       = { for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : k => v.export }
 }
 output "storage_mover_source_endpoints_host" {
-  description = "List of host values across all storage_mover_source_endpoints"
-  value       = [for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : v.host]
+  description = "Map of host values across all storage_mover_source_endpoints, keyed the same as var.storage_mover_source_endpoints"
+  value       = { for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : k => v.host }
 }
 output "storage_mover_source_endpoints_name" {
-  description = "List of name values across all storage_mover_source_endpoints"
-  value       = [for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : v.name]
+  description = "Map of name values across all storage_mover_source_endpoints, keyed the same as var.storage_mover_source_endpoints"
+  value       = { for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : k => v.name }
 }
 output "storage_mover_source_endpoints_nfs_version" {
-  description = "List of nfs_version values across all storage_mover_source_endpoints"
-  value       = [for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : v.nfs_version]
+  description = "Map of nfs_version values across all storage_mover_source_endpoints, keyed the same as var.storage_mover_source_endpoints"
+  value       = { for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : k => v.nfs_version }
 }
 output "storage_mover_source_endpoints_storage_mover_id" {
-  description = "List of storage_mover_id values across all storage_mover_source_endpoints"
-  value       = [for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : v.storage_mover_id]
+  description = "Map of storage_mover_id values across all storage_mover_source_endpoints, keyed the same as var.storage_mover_source_endpoints"
+  value       = { for k, v in azurerm_storage_mover_source_endpoint.storage_mover_source_endpoints : k => v.storage_mover_id }
 }
 
